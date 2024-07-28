d5456c0458772793ab01afcb08f7011004a29c86
beloveyebaoh <beloveyeboah@gmail.com>
2024-07-28 08:15:53 +0000

    Number template new

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -12,7 +12,7 @@ followed by the value of the text
 from flask import Flask
 from flask import render_template
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
@@ -56,12 +56,19 @@ def python(text='is cool'):
 
 @app.route('/number/<int:n>',  strict_slashes=False)
 def is_number(n):
+    """ display “n is a number” only if n is an integer
+    """
+
     return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def display_number(n):
-    return render_template('5-number.html', n=n)
+def display_number(n=None):
+    """display a HTML page only if n is an integer"""
+
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=None)
